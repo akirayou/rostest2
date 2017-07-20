0b056ed6cdd639f0dde2edfadac5dfae62839379
akira_you <you.akira.noda@gmail.com>
2017-07-21 07:19:24 +0900

    import/export/delete runs but uuid list still have bug

--- app/src/main/java/net/akirayou/rostest/SelectActivity.java
+++ app/src/main/java/net/akirayou/rostest/SelectActivity.java
@@ -11,7 +11,6 @@ import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -25,7 +24,9 @@ import com.obsez.android.lib.filechooser.ChooserDialog;
 import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SelectActivity extends AppCompatActivity {
     private static final String TAG = SelectActivity.class.getSimpleName();
@@ -61,14 +62,20 @@ public class SelectActivity extends AppCompatActivity {
                 exportADF();
             }
         });
-
         findViewById(R.id.bt_import).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 importADF();
             }
         });
-        Log.i(TAG, "==============READING ADF UUID===========");
+        findViewById(R.id.bt_del).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteADF();
+            }
+        });
+
+
 
         startActivityForResult(Tango.getRequestPermissionIntent(
                 Tango.PERMISSIONTYPE_ADF_LOAD_SAVE), Tango.TANGO_INTENT_ACTIVITYCODE);
@@ -80,8 +87,44 @@ public class SelectActivity extends AppCompatActivity {
         return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 
     }
-    protected void exportADF() {
+    interface UuidRunnable extends Runnable{
+        void setUuid(String uuid);
 
+    }
+    private boolean runOnSelectedUuid(UuidRunnable r){
+        int i = ((Spinner) findViewById(R.id.sp_uuid)).getSelectedItemPosition();
+        if ("".equals(uuids.get(i))) {
+            showsToastAndFinishOnUiThread("No uuid is selected");
+            return false;
+        } else {
+            r.setUuid(uuids.get(i));
+            r.run();
+            return true;
+        }
+    }
+    private  void deleteADF(){
+        runOnSelectedUuid(new UuidRunnable() {
+            private String uuid;
+            @Override
+            public void setUuid(String _uuid) {
+                uuid=_uuid;
+            }
+            @Override
+            public void run() {
+                MediaScannerConnection.scanFile(getApplicationContext(), new String[]{basePath().getAbsolutePath() + "/" + uuid}, null, null);
+                 runOnTango(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTango.deleteAreaDescription(uuid);
+                        showsToastAndFinishOnUiThread("DELETE:" + uuid);
+                        loadUuidList();
+                    }
+                });
+
+            }
+        });
+    }
+    protected void exportADF() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
                 PackageManager.PERMISSION_GRANTED) {
             // 以前に許諾して、今後表示しないとしていた場合は、ここにはこない
@@ -89,7 +132,7 @@ public class SelectActivity extends AppCompatActivity {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 // ユーザに許諾してもらうために、なんで必要なのかを説明する
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("なんで、そのパーミッションが必要なのかを説明");
+                builder.setMessage("Permit Exporting?");
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -98,7 +141,7 @@ public class SelectActivity extends AppCompatActivity {
                         };
                         ActivityCompat.requestPermissions(SelectActivity.this, permissions, /*REQ CODE*/ 2);
                     }});
-                builder.setNegativeButton("だめ", null);
+                builder.setNegativeButton("CANCEL", null);
                 builder.show();
             } else {
                 // startActivityForResult()みたいな感じで許諾を要求
@@ -108,16 +151,23 @@ public class SelectActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(SelectActivity.this, permissions, /*REQ CODE*/ 2);
             }
         } else {
-            int i = ((Spinner) findViewById(R.id.sp_uuid)).getSelectedItemPosition();
-            final String uuid = uuids.get(i);
-            final String file = basePath().toString() ;
-
-            MediaScannerConnection.scanFile(getApplicationContext(), new String[]{basePath().getAbsolutePath()+"/"+uuid}, null, null);
-            runInTango(new Runnable() {
+            runOnSelectedUuid(new UuidRunnable() {
+                private String uuid;
+                @Override
+                public void setUuid(String _uuid) {
+                    uuid=_uuid;
+                }
                 @Override
                 public void run() {
-                    mTango.exportAreaDescriptionFile(uuid, file);
-                    //showsToastAndFinishOnUiThread("EXPORT:" + basePath().getAbsolutePath());
+                    MediaScannerConnection.scanFile(getApplicationContext(), new String[]{basePath().getAbsolutePath() + "/" + uuid}, null, null);
+                     runOnTango(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTango.exportAreaDescriptionFile(uuid, basePath().toString());
+                            //showsToastAndFinishOnUiThread("EXPORT:" + basePath().getAbsolutePath());
+                        }
+                    });
+
                 }
             });
         }
@@ -128,14 +178,22 @@ public class SelectActivity extends AppCompatActivity {
                 .withChosenListener(new ChooserDialog.Result() {
                     @Override
                     public void onChoosePath(String path, File pathFile) {
-                        showsToastAndFinishOnUiThread( "FILE: " + path);
+                        //showsToastAndFinishOnUiThread( "FILE: " + path);
+                        final String r_path=path;
+                         runOnTango(new Runnable() {
+                            @Override
+                            public void run() {
+                                mTango.importAreaDescriptionFile(r_path);
+                            }
+                        });
+
                     }
                 })
                 .build()
                 .show();
 
     }
-    private void runInTango(final Runnable run){
+    private void runOnTango(final Runnable run){
         mTango = new Tango(this, new Runnable() {
             @Override
             public void run() {
@@ -153,13 +211,9 @@ public class SelectActivity extends AppCompatActivity {
         });
 
     }
-
-
-    @Override
-    protected void onStart(){
-        super.onStart();
+    private void loadUuidList(){
         names.clear();
-        runInTango(new Runnable() {
+         runOnTango(new Runnable() {
             @Override
             public void run() {
                 uuids = mTango.listAreaDescriptions();
@@ -171,62 +225,33 @@ public class SelectActivity extends AppCompatActivity {
                     ByteBuffer buf = ByteBuffer.wrap(epoch);
                     buf.order(ByteOrder.LITTLE_ENDIAN);
                     long epochVal = buf.getLong();
+                    String name;
                     if (nameBytes != null) {
-                        names.add(new String(nameBytes)+"-"+String.valueOf(epochVal));
-                    } else {
-                        names.add(uuids.get(i)+"-"+Long.toUnsignedString(epochVal));
+                        name = new String(nameBytes);
+                    }else {
+                        name = "NO-NAME";
                     }
-
+                    SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd_HH:mm:ss");
+                    //fmt.setTimeZone(TimeZone.getTimeZone("UTC"));
+                    names.add(name+"/"+fmt.format(new Date(epochVal))+"/"+uuids.get(i));
                 }
-
-                uuids.add("");
-                names.add("No use");
-
-                ArrayAdapter<String> ad = new ArrayAdapter<String>(SelectActivity.this, android.R.layout.simple_spinner_item, names);
+                uuids.add(0,"");
+                names.add(0,"No use");
+                ArrayAdapter<String> ad = new ArrayAdapter<String>(SelectActivity.this, android.R.layout.simple_spinner_item);
+                ad.clear();
+                ad.addAll(names);
                 ((Spinner) findViewById(R.id.sp_uuid)).setAdapter(ad);
-
+                ((Spinner) findViewById(R.id.sp_uuid)).setSelection(0);
+                ad.notifyDataSetChanged();
             }
         });
 
-/*
-        mTango = new Tango(this, new Runnable() {
-            @Override
-            public void run() {
-                synchronized (SelectActivity.this) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            uuids = mTango.listAreaDescriptions();
-                            for (int i = 0; i < uuids.size(); i++) {
-                                TangoAreaDescriptionMetaData metadata = new TangoAreaDescriptionMetaData();
-                                metadata = mTango.loadAreaDescriptionMetaData(uuids.get(i));
-                                byte[] nameBytes = metadata.get(TangoAreaDescriptionMetaData.KEY_NAME);
-                                byte[] epoch = metadata.get(TangoAreaDescriptionMetaData.KEY_DATE_MS_SINCE_EPOCH);
-                                ByteBuffer buf = ByteBuffer.wrap(epoch);
-                                buf.order(ByteOrder.LITTLE_ENDIAN);
-                                long epochVal = buf.getLong();
-                                if (nameBytes != null) {
-                                    names.add(new String(nameBytes)+"-"+String.valueOf(epochVal));
-                                } else {
-                                    names.add(uuids.get(i)+"-"+Long.toUnsignedString(epochVal));
-                                }
+    }
 
-                            }
-
-                            uuids.add("");
-                            names.add("No use");
-
-                            ArrayAdapter<String> ad = new ArrayAdapter<String>(SelectActivity.this, android.R.layout.simple_spinner_item, names);
-                            ((Spinner) findViewById(R.id.sp_uuid)).setAdapter(ad);
-                            mTango.disconnect();
-                        }
-
-                    });
-                }
-            }
-        });
-*/
-
+    @Override
+    protected void onStart(){
+        super.onStart();
+        loadUuidList();
     }
 
 
